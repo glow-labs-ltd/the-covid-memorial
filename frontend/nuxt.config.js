e58e2ba1d9287a876a8e06a98df88d91c4b714dc
kamaldaniels <kamaldaniels@gmail.com>
2021-01-04 22:24:11 +0000

    Update to node module cache. Update html head

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,11 +1,16 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
+    htmlAttrs: { lang: 'en' },
     title: 'the-covid-memorial',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'The COVID Memorial',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
