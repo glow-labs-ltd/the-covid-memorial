c147b1d2349dc0fb44227ed5abe4a2dbf4d75085
kamaldaniels <kamaldaniels@gmail.com>
2021-01-06 17:10:57 +0000

    Add static

--- backend/memorial/memorial/settings/base.py
+++ backend/memorial/memorial/settings/base.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 'static/'
 
 # Storage
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
