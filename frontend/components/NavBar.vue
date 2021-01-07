c6c4dca5fa1917b83193aaac5a12c87f42c8e60e
kamaldaniels <kamaldaniels@gmail.com>
2021-01-07 15:53:25 +0000

    Added static bottom nav

---
+++ frontend/components/NavBar.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav class="navbar">
+    <img
+      src="~/assets/images/the-covid-memorial-logo.svg"
+      class="logo"
+      alt="The COVID Memorial"
+    />
+    <h2 class="lost-count">1,854,764 <span>Sadly lost</span></h2>
+    <img src="~/assets/images/search-icon.svg" class="search" alt="Search" />
+    <img src="~/assets/images/menu-icon.svg" class="menu" alt="Menu" />
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  display: grid;
+  grid-template: auto / auto 1fr auto auto;
+  background-color: $surface;
+  color: $primary;
+  overflow: hidden;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 2rem 4rem;
+  grid-gap: 4rem;
+
+  > * {
+    align-self: center;
+  }
+
+  .logo {
+    width: 13rem;
+    height: 6rem;
+  }
+
+  .lost-count {
+    text-align: center;
+    font-size: 3.75rem;
+    font-weight: 400;
+
+    span {
+      font-size: 2.5rem;
+    }
+  }
+
+  .search {
+    width: 3.5rem;
+    height: 3.5rem;
+  }
+
+  .menu {
+    width: 5.75rem;
+    height: 3.75rem;
+  }
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,29 +1,5 @@
 <template>
   <div class="container">
-    <!--
-    <div>
-      <Logo />
-      <h1 class="title">the-covid-memorial</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-    -->
     <covid-chart />
   </div>
 </template>
