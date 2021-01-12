9b547e9d29bf0b5b6c7a6d2fa28e26404b29a8a3
kamaldaniels <kamaldaniels@gmail.com>
2021-01-12 16:05:26 +0000

    Update search

--- frontend/components/NavBar.vue
+++ frontend/components/NavBar.vue
@@ -7,24 +7,11 @@
       />
     </NuxtLink>
     <h2 class="lost-count">1,854,764 <span>Sadly lost</span></h2>
-    <div>
-      <transition name="fade-slow" mode="in-out">
-        <input
-          v-show="$store.state.showSearch"
-          ref="searchField"
-          v-model="searchQuery"
-          type="text"
-          class="search-field"
-          size="30"
-          @keyup.enter="search"
-        />
-      </transition>
-    </div>
     <img
       src="~/assets/images/search-icon.svg"
       class="search"
       alt="Search"
-      @click="toggleSearch"
+      @click="searchClick"
     />
     <img
       src="~/assets/images/menu-icon.svg"
@@ -37,30 +24,11 @@
 
 <script>
 export default {
-  computed: {
-    searchQuery: {
-      get() {
-        return this.$store.state.searchQuery
-      },
-      set(value) {
-        this.$store.commit('setSearchQuery', value)
-      },
-    },
-  },
   methods: {
     menuClick() {
       this.$store.commit('toggleSideMenu')
     },
-    toggleSearch() {
-      this.$store.commit('setShowSearch', !this.$store.state.showSearch)
-      if (this.$store.state.showSearch) {
-        this.$nextTick(() => {
-          this.$refs.searchField.focus()
-        })
-      }
-    },
-    search() {
-      this.$store.dispatch('search')
+    searchClick() {
       this.$router.push('/search')
     },
   },
@@ -70,7 +38,7 @@ export default {
 <style lang="scss" scoped>
 .navbar {
   display: grid;
-  grid-template: auto / auto 1fr auto auto auto;
+  grid-template: auto / auto 1fr auto auto;
   background-color: $surface;
   color: $primary;
   overflow: hidden;
@@ -82,7 +50,7 @@ export default {
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.5);
 
   @media (min-width: $tablet) {
-    grid-template: auto / auto 1fr auto auto auto;
+    grid-template: auto / auto 1fr auto auto;
     grid-gap: 4rem;
     padding: 2rem 4rem;
   }
@@ -127,18 +95,6 @@ export default {
   .search {
     width: 3.5rem;
     height: 3.5rem;
-  }
-
-  .search-field {
-    border: none;
-    border-bottom: 1px solid $secondary;
-    border-radius: 0;
-    max-width: 40rem;
-
-    &:focus {
-      box-shadow: 0 0 8px -4px $secondary;
-      outline: none;
-    }
   }
 
   .menu {
