0308708d839a5d9e1e37632173a5a08ad79d4a1c
Sam Carty <sam@glow.legal>
2021-01-15 11:29:55 +0000

    Increased number of dots in overview. Fixed nodes not loading initially. Tidied up code.

--- frontend/components/ChartOverview.vue
+++ frontend/components/ChartOverview.vue
@@ -18,7 +18,7 @@ export default {
       simulation: null,
       minZoom: null,
       maxZoom: null,
-      maxDots: 2000,
+      maxDots: 10000,
       radius: d3.randomInt(4, 16),
       transitionTime: 3000,
     }
@@ -26,6 +26,94 @@ export default {
   computed: mapState(['overviewTransition']),
   watch: {
     overviewTransition() {
+      this.animateOut()
+    },
+  },
+  mounted() {
+    this.canvas = d3.select('canvas')
+    this.resizeCanvas()
+    const context = this.canvas.node().getContext('2d')
+
+    const radii = Array.from({ length: 800 }, this.radius)
+    const nodes = radii.map((r) => ({ r }))
+    this.transform = d3.zoomIdentity
+    this.opacityScale = d3
+      .scaleLinear()
+      .domain([this.minZoom, this.maxZoom])
+      .range([1, 0])
+
+    this.simulation = this.setupSimulation(context, nodes)
+    this.zoom = this.setupZoom(context, nodes)
+    this.animateIn()
+
+    setTimeout(
+      function () {
+        this.spawnMoreDots(100, nodes, this.simulation)
+      }.bind(this),
+      3000
+    )
+  },
+  methods: {
+    resizeCanvas() {
+      this.width = window.innerWidth
+      this.height = window.innerHeight - 80
+      this.canvas.attr('width', this.width).attr('height', this.height)
+
+      const shortestEdge = Math.min(this.width, this.height)
+      this.minZoom = shortestEdge / 800
+      this.maxZoom = this.minZoom * 6
+
+      window.addEventListener(
+        'resize',
+        function () {
+          this.resizeCanvas()
+        }.bind(this)
+      )
+    },
+    setupSimulation(context, nodes) {
+      return d3
+        .forceSimulation(nodes)
+        .alphaTarget(0.2)
+        .velocityDecay(0.5)
+        .force('x', d3.forceX().strength(0.001))
+        .force('y', d3.forceY().strength(0.001))
+        .force(
+          'collide',
+          d3.forceCollide().radius((d) => d.r + 1)
+        )
+        .on(
+          'tick',
+          function () {
+            this.ticked(context, nodes)
+          }.bind(this)
+        )
+    },
+    setupZoom(context, nodes) {
+      const zoom = d3
+        .zoom()
+        .scaleExtent([this.minZoom, this.maxZoom])
+        .on(
+          'zoom',
+          function (event) {
+            this.transform = event.transform
+            if (this.transform.k >= this.maxZoom) {
+              this.$store.commit('setOverview', false)
+            }
+            this.ticked(context, nodes)
+          }.bind(this)
+        )
+      d3.select(this.canvas.node()).call(zoom)
+      return zoom
+    },
+    animateIn() {
+      this.canvas
+        .call(this.zoom.transform, d3.zoomIdentity.scale(this.maxZoom - 0.01))
+        .transition()
+        .delay(1500)
+        .duration(this.transitionTime)
+        .call(this.zoom.transform, d3.zoomIdentity.scale(this.minZoom))
+    },
+    animateOut() {
       this.canvas
         .transition()
         .duration(this.transitionTime)
@@ -38,74 +126,6 @@ export default {
           }.bind(this)
         )
     },
-  },
-  mounted() {
-    this.canvas = d3.select('canvas')
-    this.resizeCanvas(this.canvas)
-    const context = this.canvas.node().getContext('2d')
-
-    const radii = Array.from({ length: 800 }, this.radius)
-    const nodes = radii.map((r) => ({ r }))
-    this.transform = d3.zoomIdentity
-    this.opacityScale = d3
-      .scaleLinear()
-      .domain([this.minZoom, this.maxZoom])
-      .range([1, 0])
-
-    this.simulation = d3
-      .forceSimulation(nodes)
-      .alphaTarget(0.2)
-      .velocityDecay(0.5)
-      .force('x', d3.forceX().strength(0.001))
-      .force('y', d3.forceY().strength(0.001))
-      .force(
-        'collide',
-        d3.forceCollide().radius((d) => d.r + 1)
-      )
-      .on(
-        'tick',
-        function () {
-          this.ticked(context, nodes)
-        }.bind(this)
-      )
-
-    this.zoom = d3
-      .zoom()
-      .scaleExtent([this.minZoom, this.maxZoom])
-      .on(
-        'zoom',
-        function (event) {
-          this.transform = event.transform
-          if (this.transform.k >= this.maxZoom) {
-            this.$store.commit('setOverview', false)
-          }
-          this.ticked(context, nodes)
-        }.bind(this)
-      )
-    d3.select(this.canvas.node()).call(this.zoom)
-
-    window.addEventListener(
-      'resize',
-      function () {
-        this.resizeCanvas(this.canvas)
-      }.bind(this)
-    )
-
-    this.canvas
-      .call(this.zoom.transform, d3.zoomIdentity.scale(this.maxZoom - 0.01))
-      .transition()
-      .delay(1500)
-      .duration(this.transitionTime)
-      .call(this.zoom.transform, d3.zoomIdentity.scale(this.minZoom))
-
-    setTimeout(
-      function () {
-        this.spawnMoreDots(100, nodes, this.simulation)
-      }.bind(this),
-      3000
-    )
-  },
-  methods: {
     spawnMoreDots(interval, nodes, simulation) {
       setInterval(
         function () {
@@ -135,15 +155,6 @@ export default {
       context.fillStyle = '#333333'
       context.fill()
       context.restore()
-    },
-    resizeCanvas(canvas) {
-      this.width = window.innerWidth
-      this.height = window.innerHeight - 80
-      canvas.attr('width', this.width).attr('height', this.height)
-
-      const shortestEdge = Math.min(this.width, this.height)
-      this.minZoom = shortestEdge / 800
-      this.maxZoom = this.minZoom * 5
     },
   },
 }
